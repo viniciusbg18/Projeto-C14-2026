1a36a98a3b9758a5fd6c4d3340b8cb5600e9fd9d
viniciusbg18 <viniciusbaganhagodoy@gmail.com>
2026-09-10 20:03:15 -0300

    build: Criando aba de dependencias do JUnit

---
+++ src/test/java/org/example/teste.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class teste {
+    @Test
+
+    public void testeExemplo(){
+        assertEquals(4, 2 + 2);
+    }
+}
